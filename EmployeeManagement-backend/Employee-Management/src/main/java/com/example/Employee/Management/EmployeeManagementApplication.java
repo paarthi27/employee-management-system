9d921d2519df40a5bb12e7d0a40bf906d04010bf
Paarthibah V <paarthi2003927@gmail.com>
2026-07-02 12:49:50 +0530

    Remove Dotenv usage and simplify main method

--- EmployeeManagement-backend/Employee-Management/src/main/java/com/example/Employee/Management/EmployeeManagementApplication.java
+++ EmployeeManagement-backend/Employee-Management/src/main/java/com/example/Employee/Management/EmployeeManagementApplication.java
@@ -1,6 +1,5 @@
 package com.example.Employee.Management;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,14 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
-		 Dotenv dotenv = Dotenv.configure()
-	                .directory("src/main/resources")
-	                .ignoreIfMissing()
-	                .load();
-
-	        dotenv.entries().forEach(entry ->
-	                System.setProperty(entry.getKey(), entry.getValue())
-	        );
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
 
