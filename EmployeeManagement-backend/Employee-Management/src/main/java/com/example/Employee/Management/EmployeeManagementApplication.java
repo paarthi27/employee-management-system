a3d90eb3357cf55909db0f8dcf00fa17ad3638f9
Paarthibah V <paarthi2003927@gmail.com>
2026-07-02 11:16:42 +0530

    Configure Dotenv and use env vars

--- EmployeeManagement-backend/Employee-Management/src/main/java/com/example/Employee/Management/EmployeeManagementApplication.java
+++ EmployeeManagement-backend/Employee-Management/src/main/java/com/example/Employee/Management/EmployeeManagementApplication.java
@@ -1,5 +1,6 @@
 package com.example.Employee.Management;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeManagementApplication {
 
 	public static void main(String[] args) {
+		 Dotenv dotenv = Dotenv.configure()
+	                .directory("src/main/resources")
+	                .ignoreIfMissing()
+	                .load();
+
+	        dotenv.entries().forEach(entry ->
+	                System.setProperty(entry.getKey(), entry.getValue())
+	        );
 		SpringApplication.run(EmployeeManagementApplication.class, args);
 	}
 
